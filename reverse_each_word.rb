05974124e157799d405f86716801cde1b7603010
Michael Waldo <mbwcogic@yahoo.com>
2019-10-20 22:23:46 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+#def reverse_each_word(sentence1)
+#sentence1 = ("olleH ,ereht dna woh era ?uoy")
+#end
+
+def reverse_each_word(sentence1)
+ sentence1 = ("olleH ,ereht dna woh era ?uoy")
+  sentence1.collect do |sentence1|
+"#{sentence1}"
+  end
+end
