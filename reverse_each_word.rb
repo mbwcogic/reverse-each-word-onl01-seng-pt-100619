3eccfb3d9d624f37083ddedd15d0dc74085f1191
Michael Waldo <mbwcogic@yahoo.com>
2019-10-20 22:48:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+#def reverse_each_word(sentence1)
+#sentence1 = ("olleH ,ereht dna woh era ?uoy")
+#end
+
+#def reverse_each_word(sentence1)
+ #sentence1 = ("olleH ,ereht dna woh era ?uoy")
+  #sentence1.collect.split do |sentence1|
+#"{sentence1}"
+ # end
+#end
+
+  
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
